a4db5cd3326b15de17270a3751b0fef147fe05e3
sarmentor <rui_sarmento@hotmail.com>
2019-02-23 07:37:53 +0000

    Modifications and new files .R in main folder

--- R/RDyn.R
+++ R/RDyn.R
@@ -1,5 +1,10 @@
 library(devtools)
+library(reticulate)
 library(rPython)
+
+packages.py <- c("networkx","time","numpy","tqdm","six")
+
+py_install(packages.py, envname = "r-reticulate", method = "auto", conda = "auto")
 
 setwd("../")
 python.load(file = paste(getwd(),'/base/Python/RDyn/RDyn_v2.py',sep=""), get.exception = TRUE)
