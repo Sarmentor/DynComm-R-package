b9d0e6d8aaf40cca771e11af658a120b06e83d12
poltergeist0 <poltergeist0@XtremeGhost>
2019-03-10 20:10:01 +0000

    Converted the silly function into a proper S3 class :)
    All ready to add algorithms in other languages :o
    
    TO DO (not necessarilly on this branch or in this order):
    	- Reorganize/refactor readers to make them more universal and easier to
    use
    	- Refactor some of the readers to accept both weighted and unweighted
    edges
    	- add class to handle the setting of parameters
    	- cleanup code (remove extra debugging, comments, ...)
    	- finish implementing standalone Cpp (currently hard coded)
    	- add functions to interface of standlone to read data from files
    	- add functions to interface of R read data from Matrices
    	- add missing quality functions that are already present in the static
    louvain
    	- make tutorial about adding new algorithms and quality functions
    	- possibly remove graph initialization from constructor since it does
    not allow to correctly process errors
    	- validate inputs in R wrapper

--- R-CRAN/R/DynComm.R
+++ R-CRAN/R/DynComm.R
@@ -2,29 +2,213 @@
 
 # List of available algorithms
 ALGORITHM <- list(
-  # C++ algorithms are listed from 1 to 1000
+  # C++ algorithms are listed from 1 to 10000
   LOUVAIN=1L
   ,SHAKEN=2L
-  # Python algorithms are listed from 1001 to 2000
-  ,TILES=1001L
-  ,ETILES=1002L
+  # Python algorithms are listed from 10001 to 20000
+  ,TILES=10001L
+  ,ETILES=10002L
   )
 
 # List of available quality measures
 QUALITY <- list(
-  # C++ quality measures are listed from 1 to 1000
+  # C++ quality measures are listed from 1 to 10000
   MODULARITY=1L
   ,BALMOD=2L
-  # Python quality measures are listed from 1001 to 2000
+  # Python quality measures are listed from 10001 to 20000
 )
 
-DynComm<-function(Algorithm,Quality,parameters){
-  if(Algorithm==ALGORITHM$LOUVAIN){
-    new(DynCommR,Algorithm,Quality,parameters)
+# DynComm<-function(Algorithm,Quality,parameters){
+#   if(Algorithm==ALGORITHM$LOUVAIN){
+#     new(DynCommR,Algorithm,Quality,parameters)
+#   }
+#   else{
+#     print("Unknown algorithm :(")
+#   }
+# }
+
+# Dynom <- setClass("Dynom",slots = c(alg="numeric"))
+
+# setMethod("plot", c("Dynom", "missing"), function(x, y, ...) { alg=y })
+# setMethod("plot", "Dynom", function(x, ...) { Dynom@alg=x })
+
+# derived from example in https://www.cyclismo.org/tutorial/R/s3Classes.html
+DynComm <- function(Algorithm,Quality,parameters)
+{
+  
+  ## Get the environment for this
+  ## instance of the function.
+  # thisEnv <- environment()
+  
+  alg <- Algorithm
+  qlt <- Quality
+  prm <- parameters
+  if(alg>=1 & alg<=10000){
+    dc <- new(DynCommR,alg,qlt,prm)
+    # print(dc)
   }
   else{
     print("Unknown algorithm :(")
   }
+  # print(dc)
+
+  ## Create the list used to represent an
+  ## object for this class
+  me <- list(
+    
+    ## Define the environment where this list is defined so
+    ## that I can refer to it later.
+    # thisEnv = thisEnv,
+    
+    ## Define the accessors for the data fields.
+    # getEnv = function()
+    # {
+    #   return(get("thisEnv",thisEnv))
+    # },
+    
+    getAlgorithm = function()
+    {
+      # return(alg)
+      alg
+    },
+
+    addRemoveEdgesFile = function(graphAddRemoveFile){
+      if(alg>=1 & alg<=10000){
+        dc$addRemoveEdgesFile(graphAddRemoveFile)
+      }
+      else{
+        print("Unknown :(")
+      }
+    },
+    
+    quality=function(){
+      if(alg>=1 & alg<=10000){
+        dc$quality()
+      }
+      else{
+        print("Unknown :(")
+      }
+    },
+    
+      communityCount=function(){
+        if(alg>=1 & alg<=10000){
+          dc$communityCount()
+        }
+        else{
+          print("Unknown :(")
+        }
+      },
+    
+      communities=function(){
+        if(alg>=1 & alg<=10000){
+          dc$communities()
+        }
+        else{
+          print("Unknown :(")
+        }
+      },
+    
+    
+    communityInnerEdgesWeight=function(community){
+      if(alg>=1 & alg<=10000){
+        dc$communityInnerEdgesWeight(community)
+      }
+      else{
+        print("Unknown :(")
+      }
+    },
+    
+      communityTotalWeight=function(community){
+        if(alg>=1 & alg<=10000){
+          dc$communityTotalWeight(community)
+        }
+        else{
+          print("Unknown :(")
+        }
+      },
+      
+        
+        communityEdgeWeight=function(source,destination){
+          if(alg>=1 & alg<=10000){
+            dc$communityEdgeWeight(source,destination)
+          }
+          else{
+            print("Unknown :(")
+          }
+        },
+        
+        communityNodeCount=function(community){
+          if(alg>=1 & alg<=10000){
+            dc$communityNodeCount(community)
+          }
+          else{
+            print("Unknown :(")
+          }
+        },
+        
+        community=function(node){
+          if(alg>=1 & alg<=10000){
+            dc$community(node)
+          }
+          else{
+            print("Unknown :(")
+          }
+        },
+        
+        nodesCount=function(){
+          if(alg>=1 & alg<=10000){
+            dc$nodesCount()
+          }
+          else{
+            print("Unknown :(")
+          }
+        },
+        
+        nodesAll=function(){
+          if(alg>=1 & alg<=10000){
+            dc$nodesAll()
+          }
+          else{
+            print("Unknown :(")
+          }
+        },
+        
+          nodes=function(community){
+            if(alg>=1 & alg<=10000){
+              dc$nodes(community)
+            }
+            else{
+              print("Unknown :(")
+            }
+          },
+        
+    communityMapping = function(){
+      if(alg>=1 & alg<=10000){
+        dc$communityMapping(TRUE)
+      }
+      else{
+        print("Unknown :(")
+      }
+    },
+    
+        time=function(){
+          if(alg>=1 & alg<=10000){
+            dc$time()
+          }
+          else{
+            print("Unknown :(")
+          }
+        }
+      
+    
+  )
+  
+  ## Define the value of the list within the current environment.
+  # assign('this',me,envir=thisEnv)
+  
+  ## Set the name for the class
+  class(me) <- append(class(me),"DynComm")
+  return(me)
 }
 
 # parameters<-matrix(c("filename","test/full/as19971108.txt","-s","test/full/sequences"),2,2,TRUE)
